da2e760ce84a87c3dac5d41160590cbf27e7bc40
omercangulec <omercangulec@hotmail.com>
2023-01-05 15:25:00 +0300

    solved

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,6 +1,6 @@
 import View from './View.js';
 import icons from 'url:../../img/icons.svg';
-import { fracty } from 'fracty';
+import fracty from 'fracty';
 
 class RecipeView extends View {
   _parentElement = document.querySelector('.recipe');
